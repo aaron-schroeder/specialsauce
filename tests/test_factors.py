43d54ad4473315fe93c34685d230b0801f341e40
aaron-schroeder <aaschro@gmail.com>
2022-12-22 11:20:09 -0700

    REF: test_factors w reusable mixin

--- tests/test_factors.py
+++ tests/test_factors.py
@@ -3,32 +3,35 @@ import unittest
 from specialsauce.sources.minetti import cost_of_running, cost_of_walking
 
 
-class FactorTest:
-  pass
+class FactorTestMixin:
+  func = None
+  range_min = None
+  range_max = None
 
-class TestMinettiRun(unittest.TestCase):
+  @classmethod
+  def setUpClass(cls):
+    cls.func = staticmethod(cls.func)
+
   def test_below_range(self):
     self.assertEqual(
-      cost_of_running(-0.5),
-      cost_of_running(-0.45)
+      self.func(self.range_min),
+      self.func(self.range_min - 0.01)
     )
 
   def test_above_range(self):
     self.assertEqual(
-      cost_of_running(0.5),
-      cost_of_running(0.45)
+      self.func(self.range_max),
+      self.func(self.range_max + 0.01)
     )
 
 
-class TestMinettiWalk(unittest.TestCase):
-  def test_below_range(self):
-    self.assertEqual(
-      cost_of_walking(-0.5),
-      cost_of_walking(-0.45)
-    )
+class TestMinettiRun(FactorTestMixin, unittest.TestCase):
+  func = cost_of_running
+  range_min = -0.45
+  range_max = 0.45
 
-  def test_above_range(self):
-    self.assertEqual(
-      cost_of_walking(0.5),
-      cost_of_walking(0.45)
-    )
+
+class TestMinettiWalk(FactorTestMixin, unittest.TestCase):
+  func = cost_of_walking
+  range_min = -0.45
+  range_max = 0.45
