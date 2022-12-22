59a2b7defa16444cb5a3ea3686603b69cbe7c392
aaron-schroeder <aaschro@gmail.com>
2022-12-22 11:30:44 -0700

    Failing tests for strava + trainingpeaks interp Also added a test for massively larger grades

--- tests/test_factors.py
+++ tests/test_factors.py
@@ -1,6 +1,8 @@
 import unittest
 
 from specialsauce.sources.minetti import cost_of_running, cost_of_walking
+from specialsauce.sources.strava import gap_speed_factor
+from specialsauce.sources.trainingpeaks import ngp_speed_factor
 
 
 class FactorTestMixin:
@@ -24,6 +26,17 @@ class FactorTestMixin:
       self.func(self.range_max + 0.01)
     )
 
+  def test_percent_grade_input(self):
+    self.assertEqual(
+      self.func(self.range_max),
+      self.func(self.range_max * 100)
+    )
+
+    self.assertEqual(
+      self.func(self.range_min),
+      self.func(self.range_min * 100)
+    )
+
 
 class TestMinettiRun(FactorTestMixin, unittest.TestCase):
   func = cost_of_running
@@ -33,5 +46,17 @@ class TestMinettiRun(FactorTestMixin, unittest.TestCase):
 
 class TestMinettiWalk(FactorTestMixin, unittest.TestCase):
   func = cost_of_walking
+  range_min = -0.45
+  range_max = 0.45
+
+
+class TestNgp(FactorTestMixin, unittest.TestCase):
+  func = ngp_speed_factor
+  range_min = -0.25
+  range_max = 0.30
+
+
+class TestGap(FactorTestMixin, unittest.TestCase):
+  func = gap_speed_factor
   range_min = -0.45
   range_max = 0.45
