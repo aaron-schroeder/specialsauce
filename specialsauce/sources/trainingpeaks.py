effc47aad6f2f8201f50ab2bc181585af397d531
aaron-schroeder <aaschro@gmail.com>
2023-12-02 06:43:36 -0700

    Reorganize sources.trainingpeaks

--- specialsauce/sources/trainingpeaks.py
+++ specialsauce/sources/trainingpeaks.py
@@ -2,8 +2,6 @@ import numpy as np
 import pandas as pd
 from scipy.interpolate import interp1d
 
-from specialsauce import util
-
 
 def ngp_speed_factor(decimal_grade):
   """Calculate TrainingPeaks' NGP pace-factor as a function of percent grade.
@@ -68,18 +66,40 @@ def ngp_speed_factor(decimal_grade):
   return interp_fn(decimal_grade * 100)
 
 
-def sma(x_series, window_len, time_series=None):
-  """Simple moving average.
-  
-  Since this is used to replicate TrainingPeaks NP/NGP, I throw out the
-  first `window_len` of data (30 sec in their case).
-  https://help.trainingpeaks.com/hc/en-us/articles/204071804-Normalized-Power
+def normalize(series):
+  """Calculates the TrainingPeaks norm of a series of data.
 
-  I'm really not sure if this means they insert 0 values for the first
+  This works like a modified version of an average, based on principles
+  of human physiology.
+
+  First, a 30-second rolling average is applied to the series. 
+  Since this norm is meant to replicate TrainingPeaks NP/NGP, I follow
+  their lead in throwing out the first `window_len` of data 
+  (30 sec in their case).
+
+  Note: I'm not sure if this means they insert 0 values for the first
   30 seconds, or if they take an average of points after 30 seconds.
   My EWMA function (below) slowly increments until it gets to a
   steady-state - so it is biased to be low for short-duration bouts.
+
+  Then, the L4 norm is applied to the smoothed series. This norm 
+  emphasizes higher values by raising all values to the 4th power, 
+  averaging them, then taking the 4th root.
+  
+  Emphasizing higher values is based in the idea that best-fit curves 
+  between VO2 (oxygen consumption) and blood lactate tend to follow a 
+  4th-order relationship.
+
+  References:
+    https://help.trainingpeaks.com/hc/en-us/articles/204071804-Normalized-Power
+
   """
+  series_rolling = sma(series, 30)
+  return l4_norm(series_rolling[29:])
+
+
+def sma(x_series, window_len, time_series=None):
+  """Simple moving average, as implemented by TrainingPeaks."""
 
   if time_series is None:
     sma = x_series.rolling(window_len).mean()
@@ -99,50 +119,44 @@ def sma(x_series, window_len, time_series=None):
   return sma
 
 
-def lactate_norm(series):
-  """Calculates lactate norm of a series of data.
-
-  Unlike a simple average, the lactate norm emphasizes higher values
-  by raising all values to the 4th power, averaging them, then taking
-  the 4th root. Emphasizing higher values is rooted in the fact that
-  best-fit curves between VO2 (oxygen consumption) and lactate tend
-  to follow a 4th-order relationship.
-
-  """
+def l4_norm(series):
   return (series ** 4).mean() ** 0.25
 
 
-def ngp_speed(speed, grade):
-  return speed * ngp_speed_factor(grade)
+def training_stress_score(ngp, ftp, duration_sec):
+  """TSS"""
+  return 100 * (ngp / ftp) ** 2 * (duration_sec / 3600)
 
 
-def ewma_days(x_arr, n_days, init=0.0):
+def acute_training_load(tss_array, init=0.0):
+  """ATL
+
+  Args:
+    tss_arr (list-like): array of Training Stress Score values, assumed
+      to occur exactly [0, 1, 2, ...] days from now.
+  """
+  return ewma_days(tss_array, 7, init)
+
+
+def chronic_training_load(tss_array, init=0.0):
+  """CTL
+
+  Args:
+    tss_arr (list-like): array of Training Stress Score values, assumed
+      to occur exactly [0, 1, 2, ...] days from now.
+  """
+  return ewma_days(tss_array, 42, init)
+
+
+def ewma_days(x_array, n_days, init=0.0):
   alpha = 1 / n_days
   
   return [
     init * (1 - alpha) ** i + sum(
-      x_arr[i-n] * alpha * (1 - alpha) ** n 
-      for n in range(i+1)) 
-    for i in range(len(x_arr))
+      x_array[i - n] * alpha * (1 - alpha) ** n 
+      for n in range(i + 1)) 
+    for i in range(len(x_array))
   ]
-
-
-def acute_training_load(tss_arr, init=0.0):
-  """
-  Args:
-    tss_arr (list-like): array of Training Stress Score values, assumed
-      to occur exactly [0, 1, 2, ...] days from now.
-  """
-  return ewma_days(tss_arr, 7, init)
-
-
-def chronic_training_load(tss_arr, init=0.0):
-  """
-  Args:
-    tss_arr (list-like): array of Training Stress Score values, assumed
-      to occur exactly [0, 1, 2, ...] days from now.
-  """
-  return ewma_days(tss_arr, 42, init)
 
 
 def training_status(training_stress_balance):
@@ -156,13 +170,3 @@ def training_status(training_stress_balance):
     return 'Productive'
   else:
     return 'Overreaching'
-
-
-def training_stress_score(ngp, ftp, duration_sec):
-  """TSS"""
-  return 100 * (ngp / ftp) ** 2 * (duration_sec / 3600)
-
-
-def ngp_speed_scalar(ngp_speed_array):
-  ngp_rolling = sma(ngp_speed_array, 30)
-  return lactate_norm(ngp_rolling[29:])
